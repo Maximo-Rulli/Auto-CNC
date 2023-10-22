7b095ec2aa3da6c8ae8ba0d4bc6fa4ebed3ee2d9
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2023-10-22 11:52:42 -0300

    human data generator finished

--- human_data_ui.py
+++ human_data_ui.py
@@ -7,17 +7,19 @@ from utils import PROD_TOK, AUX_TOK, preprocess
 #Longitud máxima de una secuencia a evaluar
 MAX_LEN = 30
 
-with gr.Blocks() as my_demo:
+with gr.Blocks() as main:
     gr.Markdown("# Generador de datasets supervisados por humano con Gradio")
     choice = gr.Radio(['Piezas', 'Productos'], value='Piezas', label="Elija que tipo de dataset se generará:")
     options = gr.Radio([i for i in list(AUX_TOK.keys())], label="Elija la pieza a producir:", interactive=True)
+    num = 0 #num lleva la cuenta de ejemplos que se generaron
     
     #Se crea un dataframe para guardar los ejemplos de entrenamiento
     df = pd.DataFrame(data={'Input_seq':[None],'Output':[None],'Type':[None]})
+    df.drop([0], inplace=True)
     
     #Esta función genera una secuencia de piezas o productos aleatoriamente
     def generate(option:str):
-        global out_seq
+        global input_seq
         if option == "Piezas":
             keys = list(AUX_TOK.keys())
             
@@ -26,12 +28,12 @@ with gr.Blocks() as my_demo:
             
         #Definimos la longitud de la secuencia a generar de forma aleatoria
         seq_len = np.random.randint(2, MAX_LEN)
-        out_seq = []
+        input_seq = []
         
         for i in range(seq_len):
-            out_seq.append(np.random.choice(keys))
+            input_seq.append(np.random.choice(keys))
             
-        return out_seq #Se devuelve una lista con las/los piezas/productos generados aleatoriamente
+        return input_seq #Se devuelve una lista con las/los piezas/productos generados aleatoriamente
 
     #Esta función cambia la opción para que se puedan elegir entre piezas o productos
     def change_options(option:str):
@@ -42,11 +44,25 @@ with gr.Blocks() as my_demo:
         return options
 
     def save(option:str, selected:str):
-        global out_seq
+        global input_seq
         global df
-        row = pd.Series([out_seq, selected, option], index=df.columns)
-        df = df.append(row)
-        return df
+        global num
+        if option == "Piezas":
+            tokens = AUX_TOK
+        elif option == "Productos":
+            tokens = PROD_TOK
+        #El formato de salida de preprocess es de tensor por lo que lo pasamos a una lista
+        input_seq = preprocess(input_seq, tokens).tolist()[0]
+        selected =  preprocess([selected], tokens).tolist()[0]
+        
+        #Creamos una fila que contenga los nuevos datos
+        row = pd.DataFrame(data={'Input_seq':[input_seq], 'Output':[selected],'Type':[option]})
+        df = pd.concat([df, row], axis=0, ignore_index=True)
+
+        #Guardamos en cada iteración el documento, nunca sabemos cuando el usuario decide cerrar la aplicación
+        df.to_hdf('human_data.h5', key='df', index=False)
+        num += 1
+        return f'Datos guardados con éxito! Iteración n°: {num}'
         
     #El generator es simplemente para que cada vez que el usuario le de click se genere la secuencia
     generator = gr.Interface(generate, choice, 'text')
@@ -58,6 +74,6 @@ with gr.Blocks() as my_demo:
 
 
 if __name__ == "__main__":
-    my_demo.launch()
+    main.launch()
 
 
