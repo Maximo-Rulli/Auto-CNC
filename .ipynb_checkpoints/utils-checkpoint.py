a4b507f061e54101359a762bdbee062fe3ad7980
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2024-01-06 19:30:44 -0300

    Update of RLHF code

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -1,7 +1,7 @@
 #En este archivo declaramos funciones muy utilizadas y constantes
 import torch
 import numpy as np
-from transformers import AutoModelForSequenceClassification
+from transformers import AutoModelForSequenceClassification, AutoConfig
 
 #Declaramos como constantes los diccionarios con los tokens y las conversiones
 PROD_TOK = {"OTHER": 0, "1200": 1, "1300": 2, "1400": 3, "1600": 4, "1700": 5, "1800": 6, "1900": 7, "2100": 8, "2400": 9, "2405": 9, "2600": 10, "2800": 11, "2805": 11, "AR": 12}
@@ -46,5 +46,43 @@ def sequence_similarity(inp1:list[str], inp2:list[str], model:str, tokens:dict, 
     sent1 = preprocess(inp1, tokens).to(device)
     sent2 = preprocess(inp2, tokens).to(device)
     return sentence_distance(sent1, sent2, model)
+
+#This is the class corresponding to the custom reward model
+class RewardModel (torch.nn.Module):
+    def __init__(self, model_name:str, hidden_size:int, classes:int,*model_args, **kwargs):
+        super().__init__()
+        self.config = AutoConfig.from_pretrained(model_name)
+        self.config.vocab_size = classes+1
+        self.config.pad_token_id = classes
+        self.config.hidden_size = hidden_size
+        #print(self.config)
+        self.base = AutoModelForSequenceClassification.from_config(self.config)
+
+        #print(self.base.classifier)
+        #Last layer must be changed for it to be a regression problem
+        self.base.classifier =  torch.nn.Linear(in_features=hidden_size, out_features=1, bias=True)
+
+        #Only keep this line if the model was not pre-trained (it's a fresh BERT model)
+        #PAD = max(list(AUX_TOK.values()))+1 #Change this line according to the model (pieces/products)
+        #self.base.bert.embeddings.word_embeddings = torch.nn.Embedding(PAD+1, hidden_size, PAD)
+        #self = self.base
+
+    #Our custom model should take as input a pair of right/wrong answers with a fixed sequence
+    def forward(self, input_ids, attention_mask=None,
+                token_type_ids=None, train:bool=False):
+        #Whether the model is being trained or tested is important when inferencing
+        if train:
+            #The input_ids are structured in a way in which the first chunk corresponds to right examples and the second one to wrong ones
+            input_right, input_wrong  = input_ids[0][0], input_ids[0][1]
+            out_right = self.base(input_ids=input_right, attention_mask=attention_mask, token_type_ids=token_type_ids)
+            out_wrong = self.base(input_ids=input_wrong, attention_mask=attention_mask, token_type_ids=token_type_ids)
+            return [out_right, out_wrong]
+        else:
+            return self.base(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+
+    #Setup a custom .train() function because the base model will not train
+    def train(self, mode:bool):
+        self.base.train(mode=mode)
+        return self.base
 
 
